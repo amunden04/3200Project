16fc10fdf1de73dcecd6cb8c9c42ffb7f9e87106
amunden04 <amunden04@gmail.com>
2020-03-28 15:00:31 -0600

    still need to be tested and commented but mostly done

--- 4886.cc
+++ 4886.cc
@@ -9,7 +9,44 @@
 using namespace std;
 
 int main(){
+  int pages = 0;
+  while(cin >> pages && pages != 0){
+    bool printed[1001] = {false};
+    string r = " ";
+    cin >> r;
 
+    for(auto &c : r){
+      if(c == ','){
+        c = ' ';
+      }
+    }
+
+    istringstream iss(r);
+
+    int high, low;
+    char c;
+    while(iss >> low){
+      if(iss.get(c) && c == '-'){
+        iss >> high;
+      } else {
+        high = low;
+      }
+
+      if(low > pages){
+        continue;
+      }
+      if(high > pages){
+        high = pages;
+      }
+
+      for(int i = low; i <= high; i++){
+        printed[i] = true;
+      }
+    }
+
+    cout << count(printed, printed+1001, true) << endl;
+
+  }
 
   return 0;
 }
--- 4889.cc
+++ 4889.cc
@@ -10,6 +10,40 @@ using namespace std;
 
 int main(){
 
+  double a = 0.0;
+  double b = 0.0;
+  double c = 0.0;
+
+  while(cin >> a >> b >> c && a != 0 && b != 0 && c != 0){
+    vector<double> sizes;
+    sizes.push_back(a);
+    sizes.push_back(b);
+    sizes.push_back(c);
+    sort(sizes.begin(), sizes.end());
+
+    double length, height, thick;
+    length = sizes[2];
+    // might need to determine which one actually has a decimal
+    height = sizes[1];
+    thick = sizes[0];
+
+    if(length >= 125 && height >= 90 && thick >= 0.25){
+        if(length <= 290 && height <= 155 && thick <= 7){
+        cout << "letter" << endl;
+        } else {
+            if ( length <= 380 &&  height <= 300 && thick <= 50){
+              cout << "packet" << endl;
+        }  else {
+          if(length + (2*height + 2*thick) <= 2100){
+            cout << "parcel" << endl;
+          }
+        }
+      }
+    } else {
+      cout << "not mailable" << endl;
+    }
+
+  }
 
   return 0;
 }
