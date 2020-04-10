1e5f2d3f261f0cef760ae5725b2aad79dbb7bac6
amunden04 <amunden04@gmail.com>
2020-04-10 10:50:19 -0600

    started coding the soccer one. not sure I totally know what to do though

--- 4887.cc
+++ 4887.cc
@@ -2,13 +2,25 @@
 *  Program for Problem #4887 (Soccer) on UVALive
 *  This is from 2010 Rocky Mountain Regional Contest
 *  Written by: Amanda Munden
-*  Date: March 27, 2020
+*  Date: April 9, 2020
 */
 #include<bits/stdc++.h>
 
 using namespace std;
 
 int main(){
+  int n, m;
+
+  While(cin >> n >> m && n != 0 && m != 0){
+    string teams[20];
+    int place[20][20];
+    
+    for(int i = 0; i < n; i++){
+      cin >> teams[i];
+    }
+
+
+  }
 
 
   return 0;
